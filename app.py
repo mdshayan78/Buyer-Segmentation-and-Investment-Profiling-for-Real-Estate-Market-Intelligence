2635dec3ec6e233b07f5aa4820f8865f6df6f59a
mdshayan78 <mdshayan78@gmail.com>
2026-06-27 22:55:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import streamlit as st
+
+st.set_page_config(
+    page_title="Buyer Segmentation Dashboard",
+    page_icon="🏠",
+    layout="wide"
+)
+
+st.title("🏠 Buyer Segmentation Dashboard")
+
+st.write("Machine Learning Based Buyer Segmentation and Investment Profiling")
